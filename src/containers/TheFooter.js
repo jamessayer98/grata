9a0cd5e4af2031d16484cf9f8b3c23502d9ecc57
James Sayer <james.sayer0908@gmail.com>
2021-01-27 21:43:24 -0500

    add copyright to footer

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
 	return (
 		<CFooter fixed={false}>
 			<div>
-				<CLink href="https://coreui.io/react/docs/" target="_blank">
-					Library documentation
+				<CLink href="https://www.gratacda.com" target="_blank">
+					Grata Technologies Inc.
 				</CLink>
 			</div>
 			<div className="ml-auto">
-				<span className="mr-1">Right text</span>
+				<span className="mr-1">Copyright ©2020. All rights reserved.</span>
 			</div>
 		</CFooter>
 	);
